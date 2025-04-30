175c239e6836478d06b626799c2f7f82ae56eb65
terry yao <terryyao@apifiny.com>
2025-04-30 16:14:35 +0800

    update     allow_origins=["http://localhost:5173", "http://localhost:5174", "http://queryweb3.wmjtyd.net"],

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 # Set all CORS enabled origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://apiqueryweb3.wmjtyd.net"],
+    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://queryweb3.wmjtyd.net"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
