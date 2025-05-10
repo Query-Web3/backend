7627d2fa52a68327c5475584ba2992fc82aea2b5
terry yao <terryyao@apifiny.com>
2025-05-10 12:02:37 +0800

    add app.add_middleware domain

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 # Set all CORS enabled origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://queryweb3.wmjtyd.net"],
+    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://queryweb3.wmjtyd.net" ,"http://queryweb3.com","http://wwww.queryweb3.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
